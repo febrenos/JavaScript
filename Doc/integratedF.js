83b13ccef5c890aa62a65d6113cacbb0167183c4
Fe <54954812+febreno@users.noreply.github.com>
2022-06-14 14:07:01 -0300

    Create integratedF.js

---
+++ Doc/integratedF.js
@@ -0,0 +1,178 @@
+// ELEMENTS
+addEventListener()
+apply()
+clearInterval()
+createElement()
+createTextNode()
+getCurrentPosition()
+getElementById()
+getElementsByClassName()
+getElementsByName()
+getElementsByTagName()
+getElementsByTagNameNS()
+getItem()
+go()
+normalize()
+normalizeDocument()
+querySelector()
+querySelectorAll()
+removeAttribute()
+setInterval()
+setItem()
+setTimeout()
+showPosition()
+terminate()
+
+//NUMBER METHODS
+constructor()//Returns the function that created this object's instance. By default this is the Number object.
+toExponential()//Forces a number to display in exponential notation, even if the number is in the range in which JavaScript normally uses standard notation.
+toFixed()//Formats a number with a specific number of digits to the right of the decimal.
+toLocaleString()//Returns a string value version of the current number in a format that may vary according to a browser's locale settings.
+toPrecision()//Defines how many total digits (including digits to the left and right of the decimal) to display of a number.
+toString()
+valueOf()
+
+// BOOLEAN METHODS
+toSource()
+toString()//Returns the string representation of the number's value.
+valueOf()//Returns the number's value.
+
+// STRING METHODS
+charAt()
+charCodeAt()
+concat()
+indexOf()
+lastIndexOf()
+localeCompare()
+length()
+match()
+replace()
+search()
+slice()
+split()
+substr()
+substring()
+toLocaleLowerCase()
+toLocaleUpperCase()
+toLowerCase()
+toUpperCase()
+valueOf()
+
+// STRING HTML WRAPPERS
+anchor()
+big()
+blink()
+bold()
+fixed()
+fontcolor()
+fontsize()
+italics()
+link()
+small()
+strike()
+sub()
+sup()
+
+// ARRAY
+concat()
+every()
+filter()
+forEach()
+indexOf()
+join()
+lastIndexOf()
+map()
+pop()
+push()
+reduce()
+reduceRight()
+reverse()
+shift()
+slice()
+some()
+toSource()
+sort()
+splice()
+toString()
+unshift()
+
+//DATE
+Date()
+getDate()
+getDay()
+getFullYear()
+getHours()
+getMilliseconds()
+getMinutes()
+getMonth()
+getSeconds()
+getTime()
+getTimezoneOffset()
+getUTCDate()
+getUTCDay()
+getUTCFullYear()
+getUTCHours()
+getUTCMilliseconds()
+getUTCMinutes()
+getUTCMonth()
+getUTCSeconds()
+getYear()
+setDate()
+setFullYear()
+setHours()
+setMilliseconds()
+setMinutes()
+setMonth()
+setSeconds()
+setTime()
+setUTCDate()
+setUTCFullYear()
+setUTCHours()
+setUTCMilliseconds()
+setUTCMinutes()
+setUTCMonth()
+setUTCSeconds()
+setYear()
+toDateString()
+toGMTString()
+toLocaleDateString()
+toLocaleFormat()
+toLocaleString()
+toLocaleTimeString()
+toSource()
+toString()
+toTimeString()
+toUTCString()
+valueOf()
+
+// DATE STATIC METHODS
+Date.parse()
+Date.UTC()
+
+// MATH METHODS
+abs()//absolute value
+acos()//arccosine (in radians) 
+asin()//arcsine (in radians)
+atan()//arctangent
+atan2()//arctangent of the quotient
+ceil()//smallest integer greater
+cos()//cosine of a number
+exp()//exponential value
+floor()// largest integer less than or equal to a number
+log()//natural logarithm (base E)
+max()//largest
+min()//smallest
+pow()//base exponent
+random()//pseudo-random
+round()//number rounded
+sin()//sine 
+sqrt()//square root
+tan()//tangent of a number
+toSource()//string "Math"
+
+// REGEXP METHODS
+exec()//search for a match in its string parameter
+test()//Tests for a match in its string parameter.
+toSource()//object literal representing the specified object; you can use this value to create a new object.
+toString()//string representing the specified object.
+//https://www.tutorialspoint.com/javascript/javascript_builtin_functions.htm#
